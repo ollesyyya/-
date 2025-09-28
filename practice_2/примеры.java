b81e693ad5a24a3a774b02f48a4470412d5a6003
ollesyyya <bojkoolesa63@gmail.com>
2025-09-28 21:46:15 +0300

    Create примеры.java

---
+++ practice_2/примеры.java
@@ -0,0 +1,12 @@
+// Бинарная куча
+import java.util.PriorityQueue;
+PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+// Биномиальная куча (каркас)
+public class BinomialHeap {
+    private Node head;
+}
+// Стандартные хэш-таблицы
+import java.util.HashMap;
+import java.util.Hashtable;
+HashMap<String, Integer> map = new HashMap<>();
