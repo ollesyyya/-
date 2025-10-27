c1805438258fdc7e392e8a318010e19e8efbc8e8
ollesyyya <bojkoolesa63@gmail.com>
2025-10-27 22:21:27 +0300

    Update jv.java

--- .../jv.java
+++ .../jv.java
@@ -1,27 +1,143 @@
-public static int binarySearch(int[] array, int target) {
-    int left = 0;
-    int right = array.length - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (array[mid] == target) {
-            return mid;
+// ИНТЕРПОЛИРУЮЩИЙ ПОИСК 
+// Сложность: O(log log n) время, O(1) память
+public class InterpolationSearch {
+    public static int interpolationSearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        
+        while (low <= high && target >= arr[low] && target <= arr[high]) {
+            if (low == high) {
+                if (arr[low] == target) return low;
+                return -1;
+            }
+            
+            int pos = low + (((high - low) * (target - arr[low])) / (arr[high] - arr[low]));
+            
+            if (arr[pos] == target)
+                return pos;
+            else if (arr[pos] < target)
+                low = pos + 1;
+            else
+                high = pos - 1;
         }
-        if (array[mid] > target) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
+        return -1;
+    }
+}
+
+
+// ПОИСК ФИБОНАЧЧИ
+// Сложность: O(log n) время, O(1) память
+public class FibonacciSearch {
+    public static int fibonacciSearch(int[] arr, int target) {
+        int n = arr.length;
+        int fibMMm2 = 0;
+        int fibMMm1 = 1;
+        int fibM = fibMMm2 + fibMMm1;
+        
+        while (fibM < n) {
+            fibMMm2 = fibMMm1;
+            fibMMm1 = fibM;
+            fibM = fibMMm2 + fibMMm1;
+        }
+        
+        int offset = -1;
+        
+        while (fibM > 1) {
+            int i = Math.min(offset + fibMMm2, n - 1);
+            
+            if (arr[i] < target) {
+                fibM = fibMMm1;
+                fibMMm1 = fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+                offset = i;
+            } else if (arr[i] > target) {fibM = fibMMm2;
+                fibMMm1 = fibMMm1 - fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+            } else {
+                return i;
+            }
+        }
+        
+        if (fibMMm1 == 1 && offset + 1 < n && arr[offset + 1] == target)
+            return offset + 1;
+            
+        return -1;
+    }
+}
+
+
+// СОРТИРОВКА ВЫБОРОМ 
+// Сложность: O(n²) время, O(1) память
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
     }
-    return -1;
 }
-public static void insertionSort(int[] array) {
-    for (int i = 1; i < array.length; i++) {
-        int key = array[i];
-        int j = i - 1;
-        while (j >= 0 && array[j] > key) {
-            array[j + 1] = array[j];
-            j = j - 1;
+
+
+// СОРТИРОВКА СЛИЯНИЕМ 
+// Сложность: O(n log n) время, O(n) память
+public class MergeSort {
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            
+            merge(arr, left, mid, right);
         }
-        array[j + 1] = key;
+    }
+    
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+        
+        for (int i = 0; i < n1; i++)
+            leftArr[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            rightArr[j] = arr[mid + 1 + j];
+            
+        int i = 0, j = 0, k = left;
+        
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+        
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
     }
 }
